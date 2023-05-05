2ba22fc24c42ee91499ffc6129b05379cf386231
pazlidinov <97672036+pazlidinov@users.noreply.github.com>
2023-05-05 07:03:59 +0500

    add login, logout, home pages in accounts app

--- config/accounts/urls.py
+++ config/accounts/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from . import views
@@ -5,5 +6,8 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login/', views.login_page, name='login' ),
+    path('', views.home_page, name='home'),
+    # path('login/', views.login_page, name='login' ),
+    path("login/", auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
+    path("logout/", auth_views.LogoutView.as_view(template_name='auth/logout.html'), name='logout'),
 ]
--- config/accounts/views.py
+++ config/accounts/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def login_page(request):
-    return render(request, 'auth/login.html')
+def home_page(request):
+    return render(request, 'index.html')
